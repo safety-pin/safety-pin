2f614994938c538169ee4f6e7c919cdaac6d4e63
Daniel Kostic <danielkeske@hotmail.com>
2015-12-05 17:36:25 +0100

    Savet

---
+++ dependencies/js/savet.js
@@ -0,0 +1,20 @@
+(function(window, google){
+	
+	//map options
+	var options = {
+		center: {
+			lat: 44.802435,
+			lng: 20.466390
+		},
+		zoom: 10,
+		disabledDefaultUI: true,
+		draggable: true,
+		// maxZoom: 11,
+		// minZoom: 9
+
+	};
+	element = document.getElementById('map');
+	//map
+	map = new google.maps.Map(element, options);
+	
+}(window, google));
